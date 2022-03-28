9d40be1be082811a0f40214196675d9587a735f6
spykelion <ndilionel6@gmail.com>
2022-03-28 11:36:16 +0100

    Edit price field to Price in model Movie Also create a seedData.cs with some basic data to save in the db

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,6 +10,6 @@ namespace MvcMovie.Models
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
-        public decimal price { get; set; }
+        public decimal Price { get; set; }
         }
 }
